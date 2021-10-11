5d73bcc4cce9df01b2ef0d20a543abd06b9f941b
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-10-11 22:03:07 +0300

    feat: adaptive styles for explore section on max-width: 1920px

--- assets/script/script.js
+++ assets/script/script.js
@@ -32,18 +32,5 @@ const fillGallery = (array) => {
 
 fillGallery(images);
 
-const progressBar = document.getElementById("video_progress_bar");
-
-progressBar.addEventListener("input", function () {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
-});
-
-const progressVolume = document.getElementById("volume_bar");
-
-progressVolume.addEventListener("input", function () {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
-});
 
 console.log(`отсутствует выезжающая форма покупки билетов стоимостью 22 балла`);
---
+++ assets/script/video.js
@@ -0,0 +1,13 @@
+const progressBar = document.getElementById("video_progress_bar");
+
+progressBar.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+});
+
+const progressVolume = document.getElementById("volume_bar");
+
+progressVolume.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+});
